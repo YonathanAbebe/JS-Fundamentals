ed578b1f9826e7475d34c4410b37ce86be21d4f2
YonathanAbebe <42411197+YonathanAbebe@users.noreply.github.com>
2025-07-19 10:49:02 +0300

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,1 +1,10 @@
+const x = parseInt(process.argv[2]);
+const message = "C is fun";
 
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  for (let i = 0; i < x; i++) {
+    console.log(message);
+  }
+}
