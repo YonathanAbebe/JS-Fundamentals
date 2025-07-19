e7c9f1e5fbac7e22acf765b848118a9c9d855e9f
YonathanAbebe <42411197+YonathanAbebe@users.noreply.github.com>
2025-07-19 10:36:24 +0300

    Update 5-to_integer.js

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,1 +1,7 @@
+const args = process.argv.slice(2);
+const num = parseInt(args[0], 10);
 
+if (!isNaN(num) && args[0] !== undefined) {
+  console.log(`My number: ${num}`);
+} else {
+  console.log("Not a number");
