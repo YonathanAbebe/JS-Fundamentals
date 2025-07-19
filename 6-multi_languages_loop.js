a17fc19ac0aa8cea08f324099fa02fef2e34ffa0
YonathanAbebe <42411197+YonathanAbebe@users.noreply.github.com>
2025-07-19 10:46:54 +0300

    Update 6-multi_languages_loop.js

--- 6-multi_languages_loop.js
+++ 6-multi_languages_loop.js
@@ -1,6 +1,9 @@
-let language = ["C is fun","Python is cool","Javascript is amazing"];
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
 
-for (i=0;i<=language.length;i++)
-    {
-    console.log(language[i]);
+for (const line of lines) {
+  console.log(line);
 }
